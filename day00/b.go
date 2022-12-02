bfbec6072c8c406dab286d20ec1a3afc5c42187c
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2022-12-03 01:31:19 +0530

    improve starter template

--- day00/b.go
+++ day00/b.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func get_input() []string {
+	var lines []string
+	readFile, err := os.Open("./input.txt")
+	check(err)
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+	for fileScanner.Scan() {
+		lines = append(lines, fileScanner.Text())
+	}
+	return lines
+}
 
 func main() {
-	fmt.Println("Part b")
+	input := get_input()
+	for _, line := range input {
+		fmt.Println(line)
+	}
 }
