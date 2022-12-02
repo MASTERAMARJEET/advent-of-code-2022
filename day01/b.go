28cde99bb53c122e2d6872d950c513038253603d
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2022-12-03 01:29:24 +0530

    complete day 1

---
+++ day01/b.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func get_input() []string {
+	var lines []string
+	readFile, err := os.Open("./input.txt")
+	check(err)
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+	for fileScanner.Scan() {
+		lines = append(lines, fileScanner.Text())
+	}
+	return lines
+}
+
+func main() {
+	input := get_input()
+	maxCalories := []int{0, 0, 0}
+	elfCalories := 0
+	for _, line := range input {
+		if line == "" {
+			elfCalories = 0
+		} else {
+			calorie, err := strconv.Atoi(line)
+			check(err)
+			elfCalories += calorie
+		}
+		if elfCalories > maxCalories[0] {
+			maxCalories[0] = elfCalories
+			sort.Ints(maxCalories)
+		}
+	}
+	total := 0
+	for _, v := range maxCalories {
+		total += v
+	}
+	fmt.Println(total)
+}
